b754cf2c4b7da325c834938c9238d7861e33ea77
laura-feher <lfeher@nps.gov>
2025-08-01 09:11:17 -0400

    ✨created new functions for getting long-term slr rate from NOAA website and for calculating recent slr rate

---
+++ R/get_long_slr_rate.R
@@ -0,0 +1,42 @@
+#' Get the current park-specific "long-term" rate of sea-level rise published by
+#' NOAA tides and currents
+#'
+#' This function downloads the current "long-term" rate of sea-level rise
+#' published on the NOAA tides and currents website for the nearest tide gauge
+#' (https://tidesandcurrents.noaa.gov/sltrends/).
+#'
+#' @inheritParams get_sea_level_data
+#'
+#' @details Rates of sea-level rise reported by NOAA are (generally) updated on
+#'   an regular basis. The rates of sea-level rise (here referred to as the
+#'   long-term rate) are based on the entire period of record for each NOAA
+#'   gauge, however, the gauges vary signficantly in age and therefore this
+#'   "long-term" rate may be based on a relatively short time period for some
+#'   gauges. For example, the Ocean City MD gauge was installed in 1975 and thus
+#'   the period of record for the gauge is 1975 - present (50 years in 2025), in
+#'   contrast to the Boston gauge which was installed in 1921 and thus has a
+#'   period of record of 1921 - present (104 years in 2025).
+#'
+#' @returns A data frame with the current park-specific "long-term" rate of
+#'   sea-level rise published by NOAA.
+#'
+#' @import dplyr
+#' @importFrom readr read_csv
+#'
+#' @export
+#'
+#' @examples
+#' get_long_slr_rate(park = "ASIS")
+#'
+get_long_slr_rate <- function(park, nauset = FALSE) {
+
+    noaa_ids <- noaa_tide_gauges(park = park, nauset = nauset)
+
+    data <- as.data.frame(readr::read_csv("https://tidesandcurrents.noaa.gov/sltrends/data/USStationsLinearSeaLevelTrends.csv", show_col_types = FALSE, name_repair = make.names)) %>%
+        rename_with(., ~str_replace_all(.x, "\\.{2,}", ".")) %>%
+        rename_with(., ~str_replace_all(.x, "X", "Perc")) %>%
+        filter(Station.ID == noaa_ids$station_num) %>%
+        mutate(object_type = "long term slr rate")
+
+    return(data)
+}
---
+++ R/get_recent_slr_rate.R
@@ -0,0 +1,49 @@
+#' Calculate a park-specific recent rate of sea-level rise using sea-level data
+#' from the nearest NOAA tide gauge.
+#'
+#' This function downloads sea-level data for the nearest NOAA tide gauge from
+#' the tides and currents website (https://tidesandcurrents.noaa.gov/sltrends/)
+#' and then calculates a rate of sea-level rise for the specified time period.
+#' Defaults to the 19 year time period for the most recent tidal epoch:
+#' 2001-2019.
+#'
+#' @inheritParams get_sea_level_data
+#'
+#' @details The recent rate of sea-level rise is calculated via linear
+#'   regression using monthly mean sea-levels with the regular seasonal
+#'   fluctuations removed. Sea-level data is downloaded from the NOAA tides and
+#'   currents website https://tidesandcurrents.noaa.gov/. Note that monthly mean
+#'   sea level values are relative to the most recent mean sea-level datum
+#'   established by CO-OPS
+#'   (https://tidesandcurrents.noaa.gov/datum_options.html).The value in the
+#'   'estimate' column for the 'yr' term represents the recent rate of sea-level
+#'   rise, and the value in 'std.error' column represents the standard error of
+#'   the recent sea-level rise rate.
+#'
+#' @returns A data frame with a park-specific recent rate of sea-level rise
+#'   calculated using the sea-level data from the nearest NOAA tide gauge for
+#'   the specified time period.
+#'
+#' @seealso [get_sea_level_data()]
+#'
+#' @import dplyr
+#' @importFrom readr read_csv
+#'
+#' @export
+#'
+#' @examples
+get_recent_slr_rate <- function(park, nauset = FALSE, start_year = 2001, end_year = 2019) {
+
+    slr_data <- get_sea_level_data(park = park, nauset = nauset, start_year = start_year, end_year = end_year)
+
+    suppressWarnings(data <- slr_data %>%
+        group_by(park_code, station_num) %>%
+        nest(data = everything(.)) %>%
+        mutate(lm_mod = map(data, ~lm(Monthly_MSL_mm ~ yr, data = .x)),
+               tidied = map(lm_mod, broom::tidy),
+               glanced = map(lm_mod, broom::glance)) %>%
+        unnest(tidied, glanced) %>%
+        mutate(object_type = "recent slr rate"))
+
+    return(data)
+}
